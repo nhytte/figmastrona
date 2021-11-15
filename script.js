02d5dc63fac1a3d8a4e7a2a479ce95d69ca08cbc
Kacper Domański <30392171+nhytte@users.noreply.github.com>
2021-11-15 15:01:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function mcalert(){
+    alert("Minecraft – komputerowa gra survivalowa o otwartym świecie stworzona przez Markusa Perssona i rozwijana przez Mojang Studios. Minecraft pozwala graczom na budowanie i niszczenie obiektów położonych w losowo generowanym świecie gry. Gracz może atakować napotkane istoty, zbierać surowce czy wytwarzać przedmioty.");
+}
+
+function raftalert(){
+    alert("Raft – komputerowa gra survivalowa wyprodukowana przez studio Redbeet Interactive, a wydana przez Axolot Games 23 maja 2018 w formie wczesnego dostępu na platformie Steam.")
+}
+
+function forzaalert(){
+    alert("Forza Horizon 5 is a 2021 racing video game developed by Playground Games and published by Xbox Game Studios. It is the fifth Forza Horizon title and twelfth main instalment in the Forza series. The game is set in a fictionalised representation of Mexico. It was released on 9 November 2021 for Microsoft Windows, Xbox One, and Xbox Series X/S.");
+}
+
+function csgoalert(){
+    alert("Counter-Strike: Global Offensive (CS:GO) – wieloosobowa strzelanka pierwszoosobowa, stworzona oraz wydana przez Valve Corporation i Hidden Path Entertainment, które już wcześniej pracowały nad Counter-Strike: Source. Jest to czwarta gra z serii Counter-Strike, wydana na platformy Microsoft Windows, macOS, Xbox 360 i PlayStation 3 21 sierpnia 2012 i na Linuxa 23 września 2014.")
+}
+
+function newworldalert(){
+    alert("New World is a massively multiplayer online role-playing game (MMORPG) developed by Amazon Games Orange County and published by Amazon Games released on September 28, 2021. The game was previously scheduled to release in May 2020 and subsequently August 2021, but was delayed to its current date. Set in the mid-seventeenth century, players colonize a fictional land modeled after the Americas.")
+}
+
+function eftalert(){
+    alert("Escape from Tarkov is a hardcore and realistic online first-person action RPG with MMO features developed by Russian Saint-Petersburg-based game developer, Battlestate Games. Set in the fictional Norvinsk region located on the frontier between Russia and Europe, the metropolis of Tarkov was plunged into anarchy thanks to the Contract Wars. What was once a bustling city, is now roamed by rivalling scavenger gangs, former private military forces, and black ops units. Between them all, there's you, your stash, and the escape back to the free world from the depths of Tarkov.")
+
+}
